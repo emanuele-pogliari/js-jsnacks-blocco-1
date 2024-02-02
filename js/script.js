61a5b6d844f67f9a02005ad7abbf0407f13e9275
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-02 17:57:50 +0100

    exercise 4-5-6 completed

--- js/script.js
+++ js/script.js
@@ -27,14 +27,61 @@
 // Solo se è dispari inseriscilo nell’array.
 // Stampa a schermo il contenuto dell'array
 
-const array = [];
-let a;
+// const array = [];
+// let a;
 
-for (let i = 0; i < 6; i++) {
-    a = Number(prompt("Inserisci numero"));
-    if (a % 2 != 0) {
-        array.push(a);
-    }
+// for (let i = 0; i < 6; i++) {
+//     a = Number(prompt("Inserisci numero"));
+//     if (a % 2 != 0) {
+//         array.push(a);
+//     }
+// }
+// console.log(array);
+
+// quarto SALTATO
+// Chiedi un numero di 4 cifre all’utente
+// e calcola la somma di tutte le cifre che compongono il numero.
+
+// let a;
+// a = prompt("inserisci numero")
+// while (a.length != 4) {
+//     a = prompt("inserisci numero");
+// }
+
+// let sum = 0; ;
+// for (let i = 0; i < a.length; i++) {
+//     sum += +a[i];
+// }
+// console.log(sum);
+
+
+// BONUS
+
+// quinto
+// Stampa il cubo dei primi N numeri,
+// (partendo da 1, il numero N indicato dall'utente è compreso)
+// dove N è un numero indicato dall’utente.
+
+// let a = 3;
+
+// for (let i = 1; i <= a; i++) {
+//     console.log(i * i * i);
+// }
+
+// sesto
+// Calcola la somma e la media dei primi 10 numeri.
+
+let sum = 0;
+let i;
+let media;
+
+
+for (i = 1; i <= 10; i++) {
+    console.log(i);
+    sum += i;
 }
-console.log(array);
 
+console.log(sum);
+media = sum / i;
+const ahah = document.getElementById("snack6");
+ahah.innerHTML = media;
