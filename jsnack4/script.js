d4d9b4a2736cde42d8f9af837a291b30a9489ae6
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-05 11:58:54 +0100

    fixed exercise 4 and html style

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,14 +1,19 @@
-let a;
-a = Number(prompt("inserisci numero"));
+let num;
+num = prompt("inserisci numero");
+let savedNum = num;
 
-while (a.length != 4 || isNaN(a)) {
-    a = prompt("Numero inserito non valido, riprova");
+while (num.length != 4 || isNaN(num)) {
+    num = prompt("Numero inserito non valido, riprova");
 }
 
+let numLength = num.length;
+num = Number(num);
 let sum = 0;
 
-for (let i = 0; i < a.length; i++) {
-    sum += a % 10;
+for (let i = 0; i < numLength; i++) {
+
+    sum += num % 10;
+    num = Math.floor(num / 10);
 }
 
-document.querySelector("#result").innerHTML = `La somma dei numeri del numero ${a} e' ${sum}`;
+document.querySelector("#result").innerHTML = `La somma dei numeri del numero ${savedNum} e' ${sum}`; 
